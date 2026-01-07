c060150f471762ea30a62ac21884511d702d225a
anubsg <anubsg@gmail.com>
2026-01-07 00:01:57 -0800

    Add files via upload

--- Customer_ActivitySegmentation (1).py
+++ Customer_ActivitySegmentation (1).py
@@ -3,7 +3,7 @@
 
 # COMMAND ----------
 
-import numby as np
+import numpy as np
 import sklearn
 import pandas as pd
 import mlflow
@@ -29,7 +29,7 @@ from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_har
 # MAGIC %sh
 # MAGIC rm -r /dbfs/tmp/dff/
 # MAGIC mkdir -p /dbfs/tmp/dff/
-# MAGIC cp bank_transactions.csv /dbfs/tmp/dff/bank_transactions.csv
+# MAGIC cp bank_transactions_1.csv /dbfs/tmp/dff/bank_transactions_1.csv
 
 # COMMAND ----------
 
@@ -39,7 +39,7 @@ raw_data_path = "/tmp/dff/delta_txns"
 spark.read.option("inferSchema", "true") \
           .option("header", "true") \
           .option("delim", ",") \
-          .csv("/tmp/dff/bank_transactions.csv") \
+          .csv("dbfs:/tmp/dff/bank_transactions_1.csv") \
           .write \
           .format("delta") \
           .mode("overwrite") \
@@ -50,6 +50,37 @@ spark.read.option("inferSchema", "true") \
 # COMMAND ----------
 
 dbutils.fs.ls("dbfs:/tmp/dff/delta_txns")
+
+# COMMAND ----------
+
+# This scaling code using the simple sklearn out-of-the-box scaler. It's used here for simplicity and re-used inside our PyFunc class
+def preprocess_data(source_df,
+                    numeric_columns,
+                    fitted_scaler):
+  '''
+  Subset df with selected columns
+  Use the fitted scaler to center and scale the numeric columns  
+  '''
+  res_df = source_df[numeric_columns].copy()
+  
+  ## scale the numeric columns with the pre-built scaler
+  res_df[numeric_columns] = fitted_scaler.transform(res_df[numeric_columns])
+  
+  return res_df
+
+# COMMAND ----------
+
+df = spark.read.format("delta") \
+  .load(raw_data_path)
+
+data = df.toPandas()
+data = data.drop(columns=["CustomerID", "TransactionID", "CustomerDOB", "TransactionTime", "TransactionDate"], errors="ignore")
+
+# COMMAND ----------
+
+conda_env = mlflow.pyfunc.get_default_conda_env()
+conda_env['dependencies'][2]['pip'] += [f'sklearn=={sklearn.__version__}']
+conda_env['dependencies'][2]['pip'] += [f'scikit-learn=={sklearn.__version__}']
 
 # COMMAND ----------
 
